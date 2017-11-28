34520961a8ec5e0a3a461f13657ee0c9482e0c47
'Manchala <'pavan.manchala@capgemini.com'>
2017-11-28 11:33:48 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+	
+	class Test {
+		
+		int cal(int a, int b){
+			return(a>b? a: b);
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Test b1= new Test();
+		
+		
+		System.out.print(b1.cal(b1.cal(b1.cal(23,-32),23), 8));
+	}
+
+}
